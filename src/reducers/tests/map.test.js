ec769aab24b21ad48943bd6fe0e8c3d03de0223f
Michael Pontus <m.pontus@gmail.com>
2017-07-16 10:35:08 +0300

    Fix test runner

---
+++ src/reducers/tests/map.test.js
@@ -0,0 +1,26 @@
+import { fromJS } from 'immutable';
+import reducer from '../map';
+import { regionChange } from '../../actions/map';
+
+describe('reducer', () => {
+  it('must return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(null);
+  });
+
+  it('must handle dragging the map', () => {
+    const state = fromJS(null);
+    const action = regionChange(
+      33,
+      55,
+      0.01,
+      0.02,
+    );
+
+    expect(reducer(state, action).toJS()).toEqual({
+      latitude: 33,
+      longitude: 55,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.02,
+    });
+  });
+});
