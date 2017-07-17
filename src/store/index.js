b4c6cd55bc2df4ebe54bce751c6cfdba35af0035
Michael Pontus <m.pontus@gmail.com>
2017-07-17 16:55:41 +0300

    Enable remote debugging

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,17 @@
 import { createStore } from 'redux';
+import devToolsEnhancer from 'remote-redux-devtools';
 import reducer from './reducer';
 
 export default function configureStore(preloadedState) {
-  return createStore(reducer, preloadedState);
+  return createStore(
+    reducer,
+    preloadedState,
+    // TODO: use environment varables
+    devToolsEnhancer({
+      secure: false,
+      hostname: '192.168.1.4',
+      port: 8000,
+      suppressConnectErrors: false,
+    }),
+  );
 }
