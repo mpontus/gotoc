2a4a7eddf86a2f04198fb0bf0a8521b4d7f08507
Michael Pontus <m.pontus@gmail.com>
2017-07-25 02:58:45 +0300

    Remove businesses outside visible region

--- src/reducers/businesses.js
+++ src/reducers/businesses.js
@@ -2,14 +2,30 @@
 import { fromJS, List } from 'immutable';
 import type { Business } from 'types/Business';
 import { BUSINESSES_ADDED } from 'actions/businesses';
+import { REGION_CHANGE } from 'actions/map';
 import type { Action } from 'actions/types';
 import type { State } from './types';
 
 export default function businessesReducer(
-  state: List<Business> = List(),
+  state: List<Map<*, *>> = List(),
   action: Action,
 ) {
   switch (action.type) {
+    case REGION_CHANGE: {
+      // $FlowFixMe
+      const { region } = action.payload;
+
+      return state.filter(business => {
+        // $FlowFixMe
+        const { latitude, longitude } = business.get('coordinates').toJS();
+        const { latitudeDelta, longitudeDelta } = region;
+
+        return (
+          Math.abs(region.latitude - latitude) < latitudeDelta / 2 &&
+          Math.abs(region.longitude - longitude) < longitudeDelta / 2
+        );
+      });
+    }
     case BUSINESSES_ADDED:
       // $FlowFixMe
       return fromJS(action.payload.businesses);
