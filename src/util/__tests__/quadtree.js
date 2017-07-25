22de35c1c08c190de8b8d37b3985fbbc33f3711a
Michael Pontus <m.pontus@gmail.com>
2017-07-25 21:39:20 +0300

    Implemented Quadtree

--- src/util/__tests__/quadtree.js
+++ src/util/__tests__/quadtree.js
@@ -1,6 +1,6 @@
-import Quadtree from '../quadtree';
+import Quadtree, { Node, Boundaries } from '../quadtree';
 
-describe('quadtree', () => {
+describe('Quadtree', () => {
   it('returns points in range', () => {
     const [data1, data2, data3] = ['foo', 'bar', 'baz'];
 
@@ -12,5 +12,81 @@ describe('quadtree', () => {
     const items = qt.queryRange(25, 25, 30, 30);
 
     expect(items).toEqual([data1, data2]);
+  });
+});
+
+describe('Boundaries', () => {
+  it('must correctly detect the containment of points', () => {
+    const boundaries = new Boundaries(10, 10, 10, 10);
+
+    expect(boundaries.contains({ x: 10, y: 10 })).toBe(true);
+    expect(boundaries.contains({ x: 15, y: 15 })).toBe(true);
+    expect(boundaries.contains({ x: 10, y: 20 })).toBe(false);
+  });
+
+  it('must correctly detect rectangle intersection', () => {
+    // A overlaps B
+    expect(
+      Boundaries.create(10, 10, 10, 10).overlaps(
+        Boundaries.create(5, 5, 20, 20),
+      ),
+    ).toBe(true);
+    // B overlaps A
+    expect(
+      Boundaries.create(5, 5, 20, 20).overlaps(
+        Boundaries.create(10, 10, 10, 10),
+      ),
+    ).toBe(true);
+    // A touches B on the right side
+    expect(
+      Boundaries.create(0, 0, 10, 10).overlaps(
+        Boundaries.create(10, 0, 10, 10),
+      ),
+    ).toBe(false);
+    // A touches B on the left side
+    expect(
+      Boundaries.create(10, 0, 10, 10).overlaps(
+        Boundaries.create(0, 0, 10, 10),
+      ),
+    ).toBe(true);
+  });
+});
+
+describe('Node', () => {
+  it('returns its own points when queried', () => {
+    const node = new Node(new Boundaries(0, 0, 100, 100), [
+      { x: 10, y: 10, data: 'foo' },
+      { x: 20, y: 20, data: 'bar' },
+      { x: 30, y: 30, data: 'baz' },
+    ]);
+
+    const result = node.query(new Boundaries(20, 20, 11, 11));
+
+    expect(result).toEqual([
+      { x: 20, y: 20, data: 'bar' },
+      { x: 30, y: 30, data: 'baz' },
+    ]);
+  });
+
+  it('must query it descendants for extra points', () => {
+    const node = new Node(
+      new Boundaries(0, 0, 100, 100),
+      [{ x: 60, y: 60, data: 'foo' }],
+      [
+        new Node(new Boundaries(0, 0, 100, 50), [
+          { x: 20, y: 20, data: 'bar' },
+        ]),
+        new Node(new Boundaries(0, 50, 100, 50), [
+          { x: 70, y: 70, data: 'bar' },
+        ]),
+      ],
+    );
+
+    const result = node.query(new Boundaries(55, 55, 20, 20));
+
+    expect(result).toEqual([
+      { x: 60, y: 60, data: 'foo' },
+      { x: 70, y: 70, data: 'bar' },
+    ]);
   });
 });
--- src/util/quadtree.js
+++ src/util/quadtree.js
@@ -9,31 +9,189 @@ type Data = Point & {
   data: mixed,
 };
 
-class Quadtree {
-  static create(width: number, height: number) {
-    return new Quadtree(width, height);
+const DEFAULT_CAPACITY = 4;
+
+export class Boundaries {
+  static create(x: number, y: number, w: number, h: number) {
+    return new Boundaries(x, y, w, h);
   }
 
+  x: number;
+  y: number;
   width: number;
   height: number;
-  points: Data[] = [];
+  lazyHalfWidth: number | void;
+  lazyHalfHeight: number | void;
 
-  constructor(width: number, height: number) {
+  constructor(x: number, y: number, width: number, height: number) {
+    this.x = x;
+    this.y = y;
     this.width = width;
     this.height = height;
   }
 
-  insert(x: number, y: number, data: mixed) {
-    this.points.push({ x, y, data });
+  contains(point: Point) {
+    const { x: x1, y: y1, width: w, height: h } = this;
+    const { x: x2, y: y2 } = point;
 
-    return this;
+    return x1 <= x2 && x2 < x1 + w && y1 <= y2 && y2 < y1 + h;
   }
 
-  queryRange(x: number, y: number, width: number, height: number) {
-    return this.points
-      .filter(({ x: px, y: py }) => px - x < width && py - y < height)
-      .map(({ data }) => data);
+  overlaps(other: Boundaries) {
+    const { x: x1, y: y1, width: w1, height: h1 } = this;
+    const { x: x2, y: y2, width: w2, height: h2 } = other;
+
+    return x1 + w1 > x2 && x1 <= x2 + w2 && y1 + h1 > y2 && y1 <= y2 + h2;
+  }
+
+  get halfWidth(): number {
+    if (!this.lazyHalfWidth) {
+      this.lazyHalfWidth = this.width / 2;
+    }
+
+    return this.lazyHalfWidth;
+  }
+
+  get halfHeight(): number {
+    if (!this.lazyHalfHeight) {
+      this.lazyHalfHeight = this.height / 2;
+    }
+
+    return this.lazyHalfHeight;
+  }
+
+  get nw(): Boundaries {
+    return new Boundaries(this.x, this.y, this.halfWidth, this.halfHeight);
+  }
+
+  get ne(): Boundaries {
+    return new Boundaries(
+      this.x + this.halfWidth,
+      this.y,
+      this.halfWidth,
+      this.halfHeight,
+    );
+  }
+
+  get sw(): Boundaries {
+    return new Boundaries(
+      this.x,
+      this.y + this.halfHeight,
+      this.halfWidth,
+      this.halfHeight,
+    );
+  }
+
+  get se(): Boundaries {
+    return new Boundaries(
+      this.x + this.halfWidth,
+      this.y + this.halfHeight,
+      this.halfWidth,
+      this.halfHeight,
+    );
+  }
+
+  subdivide(): Boundaries[] {
+    return [this.nw, this.ne, this.sw, this.se];
   }
 }
 
-export default Quadtree;
+export class Node {
+  static create(boundaries: Boundaries, capacity: number = DEFAULT_CAPACITY) {
+    return new Node(boundaries, undefined, undefined, capacity);
+  }
+
+  boundaries: Boundaries;
+  capacity: number;
+  points: Data[];
+  nodes: Node[];
+
+  constructor(
+    boundaries: Boundaries,
+    points: Data[] = [],
+    nodes: Node[] = [],
+    capacity: number = DEFAULT_CAPACITY,
+  ) {
+    this.boundaries = boundaries;
+    this.points = points;
+    this.nodes = nodes;
+    this.capacity = capacity;
+  }
+
+  insert(point: Data) {
+    if (!this.boundaries.contains(point)) {
+      return this;
+    }
+
+    if (this.points.length < this.capacity) {
+      return new Node(
+        this.boundaries,
+        this.points.concat([point]),
+        undefined,
+        this.capacity,
+      );
+    }
+
+    if (this.nodes.length === 0) {
+      return this.subdivide().insert(point);
+    }
+
+    return new Node(
+      this.boundaries,
+      this.points,
+      this.nodes.map(node => node.insert(point)),
+      this.capacity,
+    );
+  }
+
+  subdivide(): Node {
+    return new Node(
+      this.boundaries,
+      this.points,
+      this.boundaries
+        .subdivide()
+        .map(boundaries => Node.create(boundaries, this.capacity)),
+      this.capacity,
+    );
+  }
+
+  query(rect: Boundaries): Data[] {
+    if (!this.boundaries.overlaps(rect)) {
+      return [];
+    }
+
+    return this.nodes.reduce(
+      (points, node) => points.concat(node.query(rect)),
+      this.points.filter(point => rect.contains(point)),
+    );
+  }
+}
+
+export default class Quadtree {
+  static create(width: number, height: number, capacity = DEFAULT_CAPACITY) {
+    return new Quadtree(
+      new Node(
+        new Boundaries(0, 0, width, height),
+        undefined,
+        undefined,
+        capacity,
+      ),
+    );
+  }
+
+  root: Node;
+
+  constructor(root: Node) {
+    this.root = root;
+  }
+
+  insert(x: number, y: number, data: mixed) {
+    return new Quadtree(this.root.insert({ x, y, data }));
+  }
+
+  queryRange(x: number, y: number, width: number, height: number) {
+    return this.root
+      .query(new Boundaries(x, y, width, height))
+      .map(({ data }) => data);
+  }
+}
