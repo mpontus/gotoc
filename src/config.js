e4668e8bd1071539516e0901361d98c4c5268720
Michael Pontus <m.pontus@gmail.com>
2017-07-27 10:59:06 +0300

    Saving test config for later

--- src/config.js
+++ src/config.js
@@ -4,9 +4,12 @@ import type { Config } from 'types/Config';
 const config: Config = {
   defaultLatitude: 37.78825,
   defaultLongitude: -122.4324,
+  // defaultLatitude: 55.7494733,
+  // defaultLongitude: 37.3523254,
   defaultRadius: 1000,
   maxConsecutiveRequests: 5,
   businessesPerAction: 150,
+  // businessesPerAction: 5,
 };
 
 export default config;
