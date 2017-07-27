9fd51cc132dde21916308d1dd6988cd9f2b60100
Michael Pontus <m.pontus@gmail.com>
2017-07-27 10:56:27 +0300

    Register points reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux-immutable';
 import createMapReducer from '../reducers/map';
 import businesses from '../reducers/businesses';
 import location from '../reducers/location';
+import points from '../reducers/points';
 
 const rootReducerFactory = config =>
   combineReducers({
     map: createMapReducer(config),
     businesses,
     location,
+    points,
   });
 
 export default rootReducerFactory;
