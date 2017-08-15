bef844749520a226eb57487a0126f853a9354a5b
Michael Pontus <m.pontus@gmail.com>
2017-08-15 11:43:10 +0300

    Remove mapSettled action

--- src/actions/map.js
+++ src/actions/map.js
@@ -2,14 +2,8 @@
 import type { Region } from 'types/Region';
 
 export const REGION_CHANGE: string = 'REGION_CHANGE';
-export const REGION_SETTLED: string = 'REGION_SETTLED';
 
 export const regionChange = (region: Region) => ({
   type: REGION_CHANGE,
-  payload: region,
-});
-
-export const regionSettled = (region: Region) => ({
-  type: REGION_SETTLED,
   payload: region,
 });
--- src/store/epic.js
+++ src/store/epic.js
@@ -1,12 +1,10 @@
 import { combineEpics } from 'redux-observable';
 // import yelpEpic from '../epics/yelp';
 import geolocationEpic from '../epics/geolocation';
-import mapEpic from '../epics/map';
 
 const rootEpic = combineEpics(
   // yelpEpic,
   geolocationEpic,
-  mapEpic,
 );
 
 export default rootEpic;
