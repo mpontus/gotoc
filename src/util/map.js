5de22e570537f167a182444a3771361e21aca233
Michael Pontus <m.pontus@gmail.com>
2017-08-16 10:16:00 +0300

    Improve boundaries resolution

--- src/util/map.js
+++ src/util/map.js
@@ -11,12 +11,22 @@ export function getRegionBoundaries(region: Region) {
     .map(d => d + longitude + 180)
     .map(l => l % 360 - 180);
 
-  // FIXME: this is not accurate
-  const [southLat, northLat] = [-1, +1]
-    .map(m => m * latitudeDelta / 2)
-    .map(d => d + latitude)
-    .map(d => Math.max(d, -85.05113))
-    .map(d => Math.min(d, +85.05113));
+  const deg2rad = Math.PI / 180;
+  const rad2deg = 180 / Math.PI;
+
+  const lat = latitude * deg2rad;
+  const latD = latitudeDelta * deg2rad;
+
+  // I don't know how this works:
+  // https://math.stackexchange.com/questions/2391556/mercator-projection-finding-boundaries-for-latitude-and-latitudedelta
+  const t = Math.tan(lat / 2 + Math.PI / 4);
+  const u = 1 / 2 * (t + 1 / t) * Math.tan(latD / 2);
+  const j = u + Math.sqrt(1 + u * u);
+  const latS = 2 * Math.atan(t / j) - Math.PI / 2;
+  const latN = 2 * Math.atan(t * j) - Math.PI / 2;
+
+  const southLat = latS * rad2deg;
+  const northLat = latN * rad2deg;
 
   return [westLng, southLat, eastLng, northLat];
 }
