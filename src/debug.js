e9c8c2b5274aba27aa5d9c24bd11e8c882b22a69
Michael Pontus <m.pontus@gmail.com>
2017-08-21 10:55:31 +0300

    Disable fixtures temporarily

--- src/debug.js
+++ src/debug.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 import { addBusinesses } from 'actions/businesses';
 import businessesFixture from '../fixtures/businesses.json';
 
 export default function(store) {
-  store.dispatch(addBusinesses(businessesFixture));
+  // store.dispatch(addBusinesses(businessesFixture));
 }
