a39a1452333fb4956cfeffde1fb134952db575cc
Michael Pontus <m.pontus@gmail.com>
2017-08-21 10:50:23 +0300

    Add mapLayout action

--- src/actions/map.js
+++ src/actions/map.js
@@ -1,9 +1,20 @@
 // @flow
 import type { Region } from 'types/Region';
 
+type Layout = {
+  width: number,
+  height: number,
+};
+
 export const REGION_CHANGE: string = 'REGION_CHANGE';
+export const LAYOUT_CHANGE: string = 'LAYOUT_CHANGE';
 
 export const regionChange = (region: Region) => ({
   type: REGION_CHANGE,
   payload: region,
+});
+
+export const mapLayout = (layout: Layout) => ({
+  type: LAYOUT_CHANGE,
+  payload: layout,
 });
