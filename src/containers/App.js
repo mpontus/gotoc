bf89dbb95de60118f64945b548c8bc3514605106
Michael Pontus <m.pontus@gmail.com>
2017-08-22 14:21:16 +0300

    Update container

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,178 +1,72 @@
 // @flow
 import React from 'react';
-import { View, Text } from 'react-native';
-import { Marker, Polyline } from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 import { connect } from 'react-redux';
-import supercluster from 'supercluster';
 import MapView from 'components/MapView';
 import { getRegion } from 'reducers/map';
-import { getLocation } from 'reducers/location';
-import { makeGetBusinessesInRegion } from 'reducers/points';
-import { regionChange } from 'actions/map';
-import { getRegionEdges, getRegionBoundaries, getZoomLevel } from 'util/map';
-import type { Location } from 'types/Location';
+import { regionChange, mapLayout } from 'actions/map';
 import type { Region } from 'types/Region';
-import type { Cluster } from 'types/Cluster';
+import type { Business } from 'types/Business';
 
-// TODO: remove debug function
-const scaleRegion = factor => region => {
-  const { latitude, longitude, latitudeDelta, longitudeDelta } = region;
+type Cluster = {|
+  cluster: true,
+  cluster_id: number,
+|};
 
-  return {
-    latitude,
-    longitude,
-    latitudeDelta: latitudeDelta * factor,
-    longitudeDelta: longitudeDelta * factor,
-  };
+type Feature = {
+  type: 'Feature',
+  properties: Cluster | (Business & { cluster: false }),
+  geometry: {
+    type: 'Point',
+    coordinates: number[],
+  },
 };
 
-const renderBoundaries = (region: Region) => {
-  const [minLng, minLat, maxLng, maxLat] = getRegionEdges(region);
-
-  const [nw, ne, sw, se] = [
-    { latitude: minLat, longitude: minLng },
-    { latitude: minLat, longitude: maxLng },
-    { latitude: maxLat, longitude: minLng },
-    { latitude: maxLat, longitude: maxLng },
-  ];
-
-  return <Polyline coordinates={[nw, ne, se, sw, nw]} strokeColor="#F00" />;
-};
-
-const mapStateToProps = () => {
-  const clustering = { rows: 4, cols: 4 };
-  const getBusinessesInRegion = makeGetBusinessesInRegion();
-
-  return state => {
-    const region = getRegion(state);
-    const activeRegion = scaleRegion(0.8)(region);
-    const businesses = getBusinessesInRegion(state, { region: activeRegion });
-    const location = getLocation(state);
-    const index = supercluster({
-      radius: 300,
-      maxZoom: 16,
-    });
-    index.load(
-      businesses.map(business => {
-        const { latitude, longitude } = business.coordinates;
-
-        return {
-          type: 'Feature',
-          properties: business,
-          geometry: {
-            type: 'Point',
-            coordinates: [longitude, latitude],
-          },
-        };
-      }),
-    );
-
-    return { region, activeRegion, location, index, clustering, businesses };
-  };
-};
-const mapDispatchToProps = { regionChange };
-const enhance = connect(mapStateToProps, mapDispatchToProps);
-
+/* Types */
 type Props = {
-  location: Location,
   region: Region,
-  activeRegion: Region,
-  clusters: Cluster[],
+  clusters: Feature[],
   regionChange: (region: Region) => void,
-  // TODO: remove debug
-  index: any,
-  businesses: Array<any>,
+  mapLayout: (layout: { width: number, height: number }) => void,
 };
 
-type State = {
-  mapWidth: ?number,
+type LayoutEvent = Event & {
+  nativeEvent: {
+    layout: {
+      width: number,
+      height: number,
+    },
+  },
 };
 
-class App extends React.Component<void, Props, State> {
-  state = {
-    mapWidth: null,
-  };
+/* Component */
+const mapStateToProps = () => state => ({
+  clusters: [],
+  region: getRegion(state),
+});
 
-  getClusters() {
-    const { mapWidth } = this.state;
-    const { index, activeRegion: region } = this.props;
+const enhance = connect(mapStateToProps, { regionChange, mapLayout });
 
-    if (!mapWidth || !region) {
-      return [];
-    }
-
-    const bbox = getRegionEdges(region);
-    const zoom = getZoomLevel(region.longitudeDelta, mapWidth);
-    const clusters = index.getClusters(bbox, zoom);
-
-    return clusters;
-  }
-
+class App extends React.Component<void, Props, void> {
   handleRegionChange = (region: Region) => {
     this.props.regionChange(region);
   };
 
-  handleMapLayout = event => {
-    const { width } = event.nativeEvent.layout;
+  handleLayout = (event: LayoutEvent) => {
+    const { width, height } = event.nativeEvent.layout;
 
-    this.setState({
-      mapWidth: width,
-    });
+    this.props.mapLayout({ width, height });
   };
 
-  renderDebug() {
-    const { mapWidth } = this.state;
-    const { region } = this.props;
-
-    if (!mapWidth || !region) {
-      return null;
-    }
-
-    const { latitude, longitude } = region;
-    const latLng = { lat: latitude, lng: longitude };
-    const zoom = getZoomLevel(region.longitudeDelta, mapWidth);
-    const { westLng, eastLng, northLat, southLat } = getRegionBoundaries(
-      latLng,
-      zoom,
-    );
-
-    return (
-      <View>
-        <View>
-          <Text>
-            westLng: {westLng}
-          </Text>
-        </View>
-        <View>
-          <Text>
-            eastLng: {eastLng}
-          </Text>
-        </View>
-        <View>
-          <Text>
-            northLat: {northLat}
-          </Text>
-        </View>
-        <View>
-          <Text>
-            southLat: {southLat}
-          </Text>
-        </View>
-      </View>
-    );
-  }
-
   render() {
-    const { region, businesses } = this.props;
-    const clusters = this.getClusters();
+    const { region, clusters } = this.props;
 
     return (
       <MapView
         region={region}
         markers={[]}
         onRegionChange={this.handleRegionChange}
-        debug={this.renderDebug()}
-        onLayout={this.handleMapLayout}
+        onLayout={this.handleLayout}
       >
         {clusters.map(cluster => {
           const { properties, geometry } = cluster;
@@ -183,23 +77,10 @@ class App extends React.Component<void, Props, State> {
             <Marker
               key={id}
               coordinate={{ latitude, longitude }}
-              pinColor="#0000FF"
-            />
-          );
-        })}
-        {businesses.map(business => {
-          const { id, coordinates } = business;
-          const { latitude, longitude } = coordinates;
-
-          return (
-            <Marker
-              key={id}
-              coordinate={{ latitude, longitude }}
               pinColor="#FF0000"
             />
           );
         })}
-        {renderBoundaries(scaleRegion(0.95)(region))}
       </MapView>
     );
   }
