520a7dcb3631fbe2110882d9e51306e64bf8999e
Michael Pontus <m.pontus@gmail.com>
2017-08-25 15:40:34 +0300

    Installed React Native Navigation
    https://wix.github.io/react-native-navigation/#/installation-android

--- android/app/src/main/java/com/gotoc/MainApplication.java
+++ android/app/src/main/java/com/gotoc/MainApplication.java
@@ -1,8 +1,9 @@
 package com.gotoc;
 
-import android.app.Application;
+import com.reactnativenavigation.NavigationApplication;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativenavigation.NavigationApplication;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -13,32 +14,23 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new ReactNativeConfigPackage(),
-            new MapsPackage()
-      );
-    }
-  };
+public class MainApplication extends NavigationApplication implements ReactApplication {
 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public boolean isDebug() {
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
+  }
+
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+      new ReactNativeConfigPackage(),
+      new MapsPackage()
+    );
   }
 
   @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
   }
 }
