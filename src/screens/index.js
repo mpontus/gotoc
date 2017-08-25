8b43128f545d8774bd06cb479a90e8256c96d579
Michael Pontus <m.pontus@gmail.com>
2017-08-25 16:38:17 +0300

    Add List screen

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 import { Navigation } from 'react-native-navigation';
 import Map from './Map';
+import List from './List';
 
 export function registerScreens(store, Provider) {
   Navigation.registerComponent('gotoc.Map', () => Map, store, Provider);
+  Navigation.registerComponent('gotoc.List', () => List, store, Provider);
 }
