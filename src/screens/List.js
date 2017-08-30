972b9ddad6259a33271fbc3ab4b0d6d58ddb7c67
Michael Pontus <m.pontus@gmail.com>
2017-08-30 10:08:41 +0300

    Implement transition to details screen

--- src/screens/List.js
+++ src/screens/List.js
@@ -1,3 +1,22 @@
-import List from 'containers/List';
+// @flow
+import React from 'react';
+import ListBusinesses from 'containers/List';
+import type { Navigator } from 'react-native-navigation';
 
-export default List;
+type Props = {
+  navigator: Navigator,
+};
+
+const ListScreen = ({ navigator }: Props) =>
+  (<ListBusinesses
+    onSelect={business =>
+      navigator.push({
+        screen: 'gotoc.Details',
+        title: business.name,
+        passProps: {
+          id: business.id,
+        },
+      })}
+  />);
+
+export default ListScreen;
