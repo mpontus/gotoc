b7313930f1c06614378afb311c50d55d8532f50d
Michael Pontus <m.pontus@gmail.com>
2017-09-02 02:37:44 +0300

    Re-enable yelp epic

--- src/store/epic.js
+++ src/store/epic.js
@@ -1,12 +1,8 @@
 import { combineEpics } from 'redux-observable';
-// import yelpEpic from '../epics/yelp';
+import yelpEpic from '../epics/yelp';
 import geolocationEpic from '../epics/geolocation';
 import reviewsEpic from '../epics/reviews';
 
-const rootEpic = combineEpics(
-  // yelpEpic,
-  geolocationEpic,
-  reviewsEpic,
-);
+const rootEpic = combineEpics(yelpEpic, geolocationEpic, reviewsEpic);
 
 export default rootEpic;
